cd3f4daea9f3d8dcf53205cc8d3c166a72475d7f
Alexanderagel <148756104+Alexanderagel@users.noreply.github.com>
2024-01-09 15:50:19 +0100

    Added TestController

--- src/main/java/com/example/_mysql_api/controllers/TestController.java
+++ src/main/java/com/example/_mysql_api/controllers/TestController.java
@@ -1,2 +1,15 @@
-package com.example._mysql_api.controllers;public class TestController {
+package com.example._mysql_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "This is a test to test the controller";
+    }
 }
+
+
