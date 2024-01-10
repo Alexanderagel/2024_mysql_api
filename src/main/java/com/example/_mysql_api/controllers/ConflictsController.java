0df85d862d708955e831054edf1ae910786443ab
Alexanderagel <148756104+Alexanderagel@users.noreply.github.com>
2024-01-10 09:46:36 +0100

    Update ConflictsController.java

--- src/main/java/com/example/_mysql_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_mysql_api/controllers/ConflictsController.java
@@ -1,9 +1,6 @@
 package com.example._mysql_api.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConflictsController {
@@ -22,6 +19,11 @@ public class ConflictsController {
 @DeleteMapping("/Conflicts")
     public String deleteConflicts(){
         return "Deleting conflicts";
+}
+
+@PutMapping("/conflicts")
+    public String putConflicts(){
+        return "Putting conflicts";
 }
 
 }
