2b108d457ab1732213a810469c19326b40b8991b
Alexanderagel <148756104+Alexanderagel@users.noreply.github.com>
2024-01-10 09:11:20 +0100

    Added Detelete conflicts method

--- src/main/java/com/example/_mysql_api/controllers/ConflictsController.java
+++ src/main/java/com/example/_mysql_api/controllers/ConflictsController.java
@@ -1,5 +1,6 @@
 package com.example._mysql_api.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,9 @@ public class ConflictsController {
         return "Getting conflicts";
     }
 
-
+@DeleteMapping("/Conflicts")
+    public String deleteConflicts(){
+        return "Deleting conflicts";
+}
 
 }
